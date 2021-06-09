02c15312d218d0a547a661ecf1d85aa2929aebbd
helly veliar <velar.helly@gmail.com>
2021-06-09 14:12:04 +0300

    Финал

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,29 @@
-crefghjhjl
+let select = function () {
+  let selectHeader = document.querySelectorAll('.language__header');
+  let selectItem = document.querySelectorAll('.language__item');
+
+  selectHeader.forEach(item => {
+    item.addEventListener('click', selectToogle)
+
+  });
+
+  selectItem.forEach(item => {
+    item.addEventListener('click', selectChoose)
+
+  });
+
+  function selectToogle() {
+    this.parentElement.classList.toggle('is-active');
+  }
+
+  function selectChoose() {
+    let text = this.innerHTML,
+      select = this.closest('.language')
+      currentText = select.querySelector('.language__current');
+    currentText.innerHTML = text;
+    select.classList.remove('is-active')
+  }
+};
+
+select();
+
